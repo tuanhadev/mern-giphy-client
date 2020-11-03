c7e6f7844682d7ea888bbb2f4b0c12a36d00cb39
Tran Van Tuan <tuanha10a4@gmail.com>
2020-11-03 12:51:40 +0700

    Updates

---
+++ precache-manifest.fca665f756bc4fe9d2ad23ebc4cb02bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dde18084af1e921771b",
+    "url": "/mern-giphy-client/static/css/main.0067b301.chunk.css"
+  },
+  {
+    "revision": "3dde18084af1e921771b",
+    "url": "/mern-giphy-client/static/js/main.3dde1808.chunk.js"
+  },
+  {
+    "revision": "71734f06c5f7d58584b8",
+    "url": "/mern-giphy-client/static/css/1.5b47db53.chunk.css"
+  },
+  {
+    "revision": "71734f06c5f7d58584b8",
+    "url": "/mern-giphy-client/static/js/1.71734f06.chunk.js"
+  },
+  {
+    "revision": "ade9dff4741a228b8196",
+    "url": "/mern-giphy-client/static/js/runtime~main.ade9dff4.js"
+  },
+  {
+    "revision": "af67f4fe6f91b8d7b199afe575319c43",
+    "url": "/mern-giphy-client/index.html"
+  }
+];
